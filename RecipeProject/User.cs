638105fa4a75e435bf458091d69599b48b544aae
odil-akhmedov <akhmedoff.odil@gmail.com>
2014-07-30 12:01:21 -0400

    added a few more recipes

--- RecipeProject/User.cs
+++ RecipeProject/User.cs
@@ -21,7 +21,7 @@ namespace RecipeProject
         }
     
         public int Id { get; set; }
-        [Display(Name = "First Name")]
+        [Display(Name = "Creator")]
         public string FirstName { get; set; }
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
